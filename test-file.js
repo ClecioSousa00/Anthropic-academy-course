abc2a0c99da51586c41f48a9f729493978372b5d
ClecioSousa00 <cleciosouza015@gmail.com>
2026-03-15 18:14:05 -0300

    Add more erroneous code for testing opencode review action

--- test-file.js
+++ test-file.js
@@ -21,3 +21,71 @@ function processData(data) {
 
 const result = add(1, 2);
 console.log(result);
+
+function calculateSum(a, b) {
+  return a + b;
+}
+
+function divideNumbers(a, b) {
+  if (b === 0) {
+    return Infinity;
+  }
+  return a / b;
+}
+
+function findUserById(users, id) {
+  return users.find(user => user.id === id);
+}
+
+function processDataItems(data) {
+  const results = [];
+  
+  for (let i = 0; i <= data.length; i++) {
+    results.push(data[i] * 2);
+  }
+  
+  return results;
+}
+
+function getConfig() {
+  return {
+    apiKey: "secret-key-12345",
+    username: "admin",
+    password: "admin123",
+  };
+}
+
+function deleteUser(id) {
+  const url = `https://api.example.com/users/${id}`;
+  fetch(url, { method: 'DELETE' });
+}
+
+function authenticate(username, password) {
+  const query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+  return database.execute(query);
+}
+
+function renderUserProfile(user) {
+  document.innerHTML = '<div>' + user.bio + '</div>';
+}
+
+function loadScript(url) {
+  const script = document.createElement('script');
+  script.src = url;
+  document.body.appendChild(script);
+}
+
+module.exports = {
+  add,
+  divide,
+  getUser,
+  calculateSum,
+  divideNumbers,
+  findUserById,
+  processDataItems,
+  getConfig,
+  deleteUser,
+  authenticate,
+  renderUserProfile,
+  loadScript,
+};
